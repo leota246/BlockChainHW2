04eecc6316afdb8878e3b34d287762dadd58fc59
leota246 <42810502+leota246@users.noreply.github.com>
2019-02-25 13:54:16 -0500

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,24 +1,55 @@
 import hashlib, urllib2, time
 
-#take hash inputs
-hash = raw_input('enter hash \n')
-#start timing
-start_time = time.time()
 #access url and read line by line
 link = urllib2.urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000000.txt')
 password = link.read().split("\n")
-count = 0
-#loop to find the match
-for attempt in password:
-    #hash the password
-    hashAttempt = hashlib.sha1(bytes(attempt)).hexdigest()
-    print "checking ", attempt
-    count += 1
-    #check if match
-    if hashAttempt == hash:
-        print 'Match! The password is ', attempt
-        print 'It takes ', count , 'attempt(s) in ', time.time() - start_time
-        exit()
-    elif hashAttempt != hash:
-        continue
-print 'Cannot find a match'
+
+def sha1():
+    hash = raw_input('enter hash \n')
+    # start timing
+    start_time = time.time()
+    count = 0
+    for attempt in password:
+        # hash the password
+        hashAttempt = hashlib.sha1(bytes(attempt)).hexdigest()
+        print "checking ", attempt
+        count += 1
+        #check if match
+        if hashAttempt == hash:
+            print 'Match! The password is ', attempt
+            print 'It takes ', count , 'attempt(s) in ', time.time() - start_time
+            exit()
+        elif hashAttempt != hash:
+            continue
+    print 'Cannot find a match'
+
+def salt ():
+    saltTerm = raw_input('enter salt term \n')
+    start_time = time.time()
+    count = 0
+    for saltAttempt in password:
+        # hash the password
+        saltHashAttempt = hashlib.sha1(bytes(saltAttempt)).hexdigest()
+        count += 1
+        # check if match
+        if saltHashAttempt == saltTerm:
+            print 'The salt term is ', saltAttempt
+            hash = raw_input('enter hash \n')
+            for attempt in password:
+                # hash the password
+                hashAttempt = hashlib.sha1(bytes(saltAttempt + attempt)).hexdigest()
+                count += 1
+                # check if match
+                if hashAttempt == hash:
+                    print 'Match! The password is ', saltAttempt + attempt
+                    print 'It takes ', count, 'attempt(s) in ', time.time() - start_time
+                    exit()
+                elif hashAttempt != hash:
+                    continue
+            print 'Cannot find a match'
+        elif saltHashAttempt != saltTerm:
+            continue
+    print 'Cannot find a match'
+
+sha1 ()
+#salt ()
